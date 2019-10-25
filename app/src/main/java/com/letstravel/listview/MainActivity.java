50b15a4946e17d4263ad7bd96c43cce8c4c20b97
rmfadera10 <55060246+rmfadera10@users.noreply.github.com>
2019-10-25 10:27:22 +0545

    List view using java

--- app/src/main/java/com/letstravel/listview/MainActivity.java
+++ app/src/main/java/com/letstravel/listview/MainActivity.java
@@ -3,12 +3,30 @@ package com.letstravel.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+private ListView listView;
 
+public static final String Countries[]={
+
+        "Nepal",
+        "Japan",
+        "India",
+        "Chiana",
+        "USA",
+};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView=(ListView)findViewById(R.id.listview1);
+
+
+        ArrayAdapter<String> arrayAdapter1=new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1,Countries);
+        listView.setAdapter(arrayAdapter1);
     }
 }
